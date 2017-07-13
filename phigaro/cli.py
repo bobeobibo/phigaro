004222f5d8623e1407828c1ba90e4738c6c9728e
Nikita <iamlilacpenguin@gmail.com>
2017-07-13 12:57:17 +0300

    basic cli

--- phigaro/cli.py
+++ phigaro/cli.py
@@ -1,6 +1,15 @@
-def main():
-    pass
+from __future__ import absolute_import
 
+import argparse
+from phigaro import phigaro
+
+
+def main():
+    p = argparse.ArgumentParser(description="Tool for predicting prophages in metagenomes")
+    p.add_argument('npn_file')
+
+    args = p.parse_args()
+    phigaro.find_phage(args)
 
 if __name__ == '__main__':
     main()
