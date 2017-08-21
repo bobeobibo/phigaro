ab5d82d8d2f8f9ce6d3063a6b2a52ac93ffc15e3
Nikita <iamlilacpenguin@gmail.com>
2017-08-21 14:52:51 +0300

    hmmer_task: removing comments

--- phigaro/scheduling/task/hmmer.py
+++ phigaro/scheduling/task/hmmer.py
@@ -39,55 +39,5 @@ class HmmerTask(AbstractTask):
                    '--tblout', self.output(),
                    self.config['hmmer']['pvog_path'],
                    self.gene_mark.output())
-        # self._parse_hmmer_output()
 
-    # @staticmethod
-    # def parse_line(line):
-    #     tokens = re.split(r'\s+', line)
-    #     scaffold = '>' + line.split('>')[1]
-    #     name = tokens[0]
-    #     evalue = float(tokens[4])
-    #     return scaffold, name, evalue
-    #
-    # def _parse_hmmer_output(self):
-    #     max_evalue = self.config['hmmer']['e_value_threshold']
-    #
-    #     with open(self._tmp_hmmer_out) as f:
-    #         it = (
-    #             self.parse_line(line.strip())
-    #             for line in f
-    #             if not line.startswith('#') and line.strip()
-    #         )
-    #
-    #         hmm_res = {}
-    #         for scaffold, name, evalue in it:
-    #             if scaffold not in hmm_res:
-    #                 hmm_res[scaffold] = {}
-    #             hmm_res[scaffold][name] = 1 if evalue < max_evalue else 0
-    #
-    #     with open(self.gene_mark.output()) as f:
-    #         with open(self.output(), 'w') as of:
-    #             writer = csv.writer(of, delimiter='\t')
-    #             it = (
-    #                 line.strip().split('\t')
-    #                 for line in f
-    #                 if line.startswith('>')
-    #             )
-    #
-    #             for scaffold, group in groupby(it, key=lambda t: t[1]):
-    #                 if scaffold not in hmm_res:
-    #                     # logger.warning('Scaffold not in hmmer out: {}'.format(scaffold))
-    #                     continue
-    #                 scaffold_res = hmm_res[scaffold]
-    #
-    #                 is_phage_it = (
-    #                     scaffold_res.get(name[1:], 0)
-    #                     for name, _ in group
-    #                 )
-    #
-    #                 is_phage_it = (
-    #                     'P' if is_phage else 'N'
-    #                     for is_phage in is_phage_it
-    #                 )
-    #                 writer.writerow((scaffold, ''.join(is_phage_it)))
 
