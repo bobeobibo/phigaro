d8704e6c341bb4d863a26c851d756c6096bbecbf
pollytikhonova <tikhonova.polly@mail.ru>
2020-01-28 20:43:57 +0300

    py3.sh edits

--- test_data_gitaction/test_python3.sh
+++ test_data_gitaction/test_python3.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-conda env list
-conda activate python3
+/root/miniconda3/condabin/conda env list
+/root/miniconda3/condabin/conda activate python3
+which pip
 pip install -r /home/latest_version.txt
 phigaro -f /home/test_short_seq.fasta -o output -e html txt gff bed --not-open -d
 pip uninstall phigaro 
