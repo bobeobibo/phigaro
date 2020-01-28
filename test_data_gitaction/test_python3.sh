e6a7b73cc87c82f2117b570739a4c3019231c905
pollytikhonova <tikhonova.polly@mail.ru>
2020-01-28 20:35:04 +0300

    edit test_python

--- test_data_gitaction/test_python3.sh
+++ test_data_gitaction/test_python3.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
 conda env list
 conda activate python3
+pip install -r /home/latest_version.txt
+phigaro -f /home/test_short_seq.fasta -o output -e html txt gff bed --not-open -d
+pip uninstall phigaro 
+conda deactivate
 echo Phigaro works fine at python3
